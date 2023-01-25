fcb709f38244f01897c0b68ff2119ec9470a1da8
raisulislamg4 <raisulislamg4@gmail.com>
2023-01-26 02:11:17 +0600

    admin dashboard created with css

--- admin_home.php
+++ admin_home.php
@@ -14,9 +14,20 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Admin Dashboard</title>
+
+    <link rel="stylesheet" type="text/css" href="css/admin.css">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 </head>
 <body>
-    <h2>Admin Dashboard</h2>
-    <a href="logout.php">Logout</a>
+    <header class="header">
+        <a href="">Admin Dashboard</a>
+        <div class="logout">
+            <a class="btn btn-primary" href="logout.php">Logout</a>
+        </div>
+    </header>
 </body>
 </html>
