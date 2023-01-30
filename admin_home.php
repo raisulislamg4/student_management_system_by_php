56a79f53138cb762701e78ed9c5d84339f052f3c
msalahuddin267 <msalahuddin267@gmail.com>
2023-01-31 03:15:56 +0600

    admin home, student home dashboard page designed

--- admin_home.php
+++ admin_home.php
@@ -29,5 +29,30 @@
             <a class="btn btn-primary" href="logout.php">Logout</a>
         </div>
     </header>
+    <aside>
+        <ul>
+            <li>
+                <a href="">Addmission</a>
+            </li>
+            <li>
+                <a href="">Add Student</a>
+            </li>
+            <li>
+                <a href="">View Student</a>
+            </li>
+            <li>
+                <a href="">Add Teacher</a>
+            </li>
+            <li>
+                <a href="">View Teacher</a>
+            </li>
+            <li>
+                <a href="">Add Courses</a>
+            </li>
+            <li>
+                <a href="">View Courses</a>
+            </li>
+        </ul>
+    </aside>
 </body>
 </html>
--- student_home.php
+++ student_home.php
@@ -14,9 +14,30 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Student Dashboard</title>
+
+    <link rel="stylesheet" type="text/css" href="css/student.css">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 </head>
 <body>
-    <h4>Student Dashboard</h4>
-    <a href="logout.php">Logout</a>
+    <header class="header">
+        <a href="">Student Dashboard</a>
+        <div class="logout">
+            <a class="btn btn-primary" href="logout.php">Logout</a>
+        </div>
+    </header>
+    <aside>
+        <ul>
+            <li>
+                <a href="">My Courses</a>
+            </li>
+            <li>
+                <a href="">My Results</a>
+            </li>
+        </ul>
+    </aside>
 </body>
 </html>
