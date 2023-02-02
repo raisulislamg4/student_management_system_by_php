db4f2511a53a30bf11c4f10e306617dba42c23c6
Raisul Islam <raisulislamg4@gmail.com>
2023-02-02 15:14:56 +0600

    admission application submit feature done

--- admission_form_check.php
+++ admission_form_check.php
@@ -1,5 +1,26 @@
 <?php 
 
-require_once "db_con.php"
+require_once "db_con.php";
 
- ?>
+if (isset($_POST['apply'])) {
+    
+    $name_data = $_POST['name'];
+    $email_data = $_POST['email'];
+    $phone_data = $_POST['phone'];
+    $message_data = $_POST['message'];
+
+    $sql="INSERT INTO ADMISSIONS(NAME, EMAIL, PHONE, MESSAGE)
+            VALUES('$name_data', '$email_data', '$phone_data', '$message_data')";
+
+    $result = mysqli_query($data, $sql);
+
+    if ($result) {
+        echo "Your Application Has Submitted Successfully!";
+    }
+    else{
+        echo "Apply Failed! Please Try Again Later";
+    }
+
+}
+
+?>
