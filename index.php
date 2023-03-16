9a48b81abcb2c4c99fb76f818363aa48f1b155d0
shahin-dev963 <shahindev963@gmail.com>
2023-03-17 05:34:35 +0600

    broken issue by nazrul's commit fixed

--- index.php
+++ index.php
@@ -64,79 +64,19 @@ if ($_SESSION['message']) {
 	<center>
 		<h1 class="section_div">Our Teachers</h1>
 	</center>
-	<<div class="content">
-	<div class="container-fluid">
-		<div>   
-			<a href="#"><strong><span class="ti-crown"></span> Teachers Section</strong></a>
-			<hr>		
-			<div class="panel-heading">
-				<div class="row">
-					<div class="col-md-10">
-						<h3 class="panel-title"></h3>
-					</div>
-					<div class="col-md-2" align="right">
-						<button type="button" name="add" id="addTeacher" class="btn btn-success btn-xs">Add New Teacher</button>
-					</div>
-				</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4">
+				<img class="teacher" src="image/teacher.png">
+				<p>We always try to teach trending courses. Our goal is to keep ourselves up to date in this new technology era.</p>
 			</div>
-			<table id="teacherList" class="table table-bordered table-striped">
-				<thead>
-					<tr>
-						<th>ID</th>
-						<th>Name</th>	
-						<th>Assigned Subjects</th>	
-						<th>Class</th>	
-						<th>Sections</th>								
-						<th></th>
-						<th></th>							
-					</tr>
-				</thead>
-			</table>
-			
-		</div>			
-	</div>		
-
-	public function listTeacher(){		
-	$sqlQuery = "SELECT t.teacher_id, t.teacher, s.subject, c.name, se.section			
-		FROM ".$this-<teacherTable." as t 
-		LEFT JOIN ".$this-<subjectsTable." as s ON t.subject_id = s.subject_id
-		LEFT JOIN ".$this-<classesTable." as c ON t.teacher_id = c.teacher_id
-		LEFT JOIN ".$this-<sectionsTable." as se ON c.section = se.section_id ";
-	if(!empty($_POST["search"]["value"])){
-		$sqlQuery .= ' WHERE (t.teacher_id LIKE "%'.$_POST["search"]["value"].'%" ';
-		$sqlQuery .= ' OR t.teacher LIKE "%'.$_POST["search"]["value"].'%" ';					
-	}
-	if(!empty($_POST["order"])){
-		$sqlQuery .= 'ORDER BY '.$_POST['order']['0']['column'].' '.$_POST['order']['0']['dir'].' ';
-	} else {
-		$sqlQuery .= 'ORDER BY t.teacher_id DESC ';
-	}
-	if($_POST["length"] != -1){
-		$sqlQuery .= 'LIMIT ' . $_POST['start'] . ', ' . $_POST['length'];
-	}	
-	$result = mysqli_query($this-<dbConnect, $sqlQuery);
-	$numRows = mysqli_num_rows($result);
-	$teacherData = array();	
-	while( $teacher = mysqli_fetch_assoc($result) ) {		
-		$teacherRows = array();			
-		$teacherRows[] = $teacher['teacher_id'];
-		$teacherRows[] = $teacher['teacher'];
-		$teacherRows[] = $teacher['subject'];
-		$teacherRows[] = $teacher['name'];	
-		$teacherRows[] = $teacher['section'];				
-		$teacherRows[] = '>button type="button" name="update" id="'.$teacher["teacher_id"].'" class="btn btn-warning btn-xs update"<Update>/button<';
-		$teacherRows[] = '>button type="button" name="delete" id="'.$teacher["teacher_id"].'" class="btn btn-danger btn-xs delete" <Delete>/button<';
-		$teacherData[] = $teacherRows;
-	}
-	$output = array(
-		"draw"				=<	intval($_POST["draw"]),
-		"recordsTotal"  	=<  $numRows,
-		"recordsFiltered" 	=< 	$numRows,
-		"data"    			=< 	$teacherData
-	);
-	echo json_encode($output);
-}
-</div>	
+			<div class="col-md-4">
+				<img class="teacher" src="image/teacher.png">
+				<p>We always try to teach trending courses. Our goal is to keep ourselves up to date in this new technology era.</p>
+			</div>
+			<div class="col-md-4">
+				<img class="teacher" src="image/teacher.png">
+				<p>We always try to teach trending courses. Our goal is to keep ourselves up to date in this new technology era.</p>
 			</div>
 		</div>
 	</div>
